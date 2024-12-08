c03b40e7d7a20e9e580347e7e1677f58df4a7739
zubayr <abdussalamazzubayr@gmail.com>
2024-12-08 07:36:50 +0100

    fix(app): Clerk done

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar.jsx";
 import { Poppins } from "next/font/google";
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import { StateContext } from "../context/StateContext.js";
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 const poppins_font = Poppins({
   weight: "400",
   subsets: ["latin"],
@@ -13,13 +14,25 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <StateContext>
         <body className={poppins_font.className}>
+        <header>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          <main>
           <Navbar />
           <NuqsAdapter>{children}</NuqsAdapter>
+          </main>
         </body>
       </StateContext>
     </html>
+    </ClerkProvider>
   );
 }
